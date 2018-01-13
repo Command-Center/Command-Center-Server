ecaa948f791d62d82bd515e5827240fb72d0e693
Steinar Malin <sma118@post.uit.no>
2018-01-13 10:02:33 +0100

    lagt til sensorhandlers

--- app.py
+++ app.py
@@ -31,13 +31,82 @@ class TemperatureSocketHandler(tornado.websocket.WebSocketHandler):
             time.sleep(2)
     def on_close(self):
         self.sending = False
+class HumiditySocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        self.sense = SenseHat()
+        self.sense.clear()
+        self.sending = True
+    def on_message(self, message):
+        num = 0
+        while(self.sending and num < 10):
+            num = num + 1
+            humidity = self.sense.get_humidity()
+            self.write_message(str(humidity))
+            time.sleep(2)
+    def on_close(self):
+        self.sending = False
+class PressureSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        self.sense = SenseHat()
+        self.sense.clear()
+        self.sending = True
+    def on_message(self, message):
+        num = 0
+        while(self.sending and num < 10):
+            num = num + 1
+            pressure = self.sense.get_pressure()
+            self.write_message(str(pressure))
+            time.sleep(2)
+    def on_close(self):
+        self.sending = False
+class OrientationSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        self.sense = SenseHat()
+        self.sense.clear()
+        self.sending = True
+    def on_message(self, message):
+        num = 0
+        while(self.sending and num < 10):
+            num = num + 1
+            orientation = self.sense.get_orientation()
+            pitch = str(orientation["pitch"])
+            roll = str(orientation["roll"])
+            yaw = str(orientation["yaw"])
 
+            orientations = pitch + " " + roll + " " + yaw
+            self.write_message(orientations)
+            time.sleep(0.1)
+    def on_close(self):
+        self.sending = False
+class AccelerationSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        self.sense = SenseHat()
+        self.sense.clear()
+        self.sending = True
+    def on_message(self, message):
+        num = 0
+        while(self.sending and num < 10):
+            num = num + 1
+            acceleration = sense.get_accelerometer_raw()
+            x = str(acceleration['x'])
+            y = str(acceleration['y'])
+            z = str(acceleration['z'])
+
+            accelerations = x + " " + y + " " + z
+            self.write_message(accelerations)
+            time.sleep(0.1)
+    def on_close(self):
+        self.sending = False
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r'/', IndexPageHandler),
             (r'/websocket', WebSocketHandler),
-            (r'/temp', TemperatureSocketHandler)
+            (r'/temp', TemperatureSocketHandler),
+            (r'/pressure', PressureSocketHandler),
+            (r'/humidity', HumiditySocketHandler),
+            (r'/orientation', OrientationSocketHandler),
+            (r'/acceleration', AccelerationSocketHandler)
         ]
 
         settings = {
