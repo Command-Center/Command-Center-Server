afd27d3bf033d1e7e0bc4eb2343e4e736f291a3f
Steinar Malin <sma118@post.uit.no>
2018-01-13 05:43:24 +0000

    lagt til temphandler

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import tornado.websocket
 import tornado.httpserver
 import tornado.ioloop
 
+from sense_hat import SenseHat
+import time
+
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         pass
@@ -14,12 +17,27 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 class IndexPageHandler(tornado.web.RequestHandler):
     def get(self):
        self.render("index.html")
+class TemperatureSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        self.sense = SenseHat()
+        self.sense.clear()
+        self.sending = True
+    def on_message(self, message):
+        num = 0
+        while(self.sending and num < 10):
+            num = num + 1
+            temp = self.sense.get_temperature()
+            self.write_message(str(temp))
+            time.sleep(2)
+    def on_close(self):
+        self.sending = False
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r'/', IndexPageHandler),
-            (r'/websocket', WebSocketHandler)
+            (r'/websocket', WebSocketHandler),
+            (r'/temp', TemperatureSocketHandler)
         ]
 
         settings = {
