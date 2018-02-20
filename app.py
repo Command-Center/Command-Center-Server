bbc2844445742eae585c2b40d731007f608c3488
Steinar Malin <sma118@post.uit.no>
2018-02-20 12:57:03 +0000

    lagt til irtemp readings

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from tornado import gen
 
 from sense_hat import SenseHat
 import time
+import minimalmodbus
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
@@ -26,7 +27,7 @@ class TemperatureSocketHandler(tornado.websocket.WebSocketHandler):
             temp = self.sense.get_temperature()
             yield self.write_message(str(temp))
             print(str(temp))
-            yield gen.sleep(2)
+            yield gen.sleep(5)
     def open(self):
         print("Temperature socket opened")
         self.sense = SenseHat()
@@ -42,6 +43,33 @@ class TemperatureSocketHandler(tornado.websocket.WebSocketHandler):
     def on_close(self):
         print("temperature socket closed")
         self.sending = False
+class IRTemperatureSocketHandler(tornado.websocket.WebSocketHandler):
+    @gen.coroutine
+    def async_write(self):
+        print("ASYNC FUNC CALLED")
+        while(self.sending):
+            try:
+                temp = self.instrument.read_register(16, 1)
+                yield self.write_message(str(temp))
+                yield gen.sleep(5)
+            except Exception:
+                yield gen.sleep(5)
+                pass
+    def open(self):
+        print("IRTemperature socket opened")
+        self.instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
+        self.instrument.baudrate = 9600
+        self.sending = False
+    def on_message(self, message):
+        print("ON_MESSAGE: TEMP")
+        if(message == "START"):
+            self.sending = True
+            tornado.ioloop.IOLoop.current().add_future(self.async_write(), lambda f: self.close())
+        if(message == "STOP"):
+            self.sending = False
+    def on_close(self):
+        print("IRTemperature socket closed")
+        self.sending = False
 class HumiditySocketHandler(tornado.websocket.WebSocketHandler):
     @gen.coroutine
     def async_write(self):
@@ -50,7 +78,7 @@ class HumiditySocketHandler(tornado.websocket.WebSocketHandler):
             humidity = self.sense.get_humidity()
             yield self.write_message(str(humidity))
             print(str(humidity))
-            yield gen.sleep(2)
+            yield gen.sleep(10)
     def open(self):
         print("Humidity socket opened")
         self.sense = SenseHat()
@@ -64,7 +92,7 @@ class HumiditySocketHandler(tornado.websocket.WebSocketHandler):
         if(message == "STOP"):
             self.sending = False
     def on_close(self):
-        print("Humidity" socket closed")
+        print("Humidity socket closed")
         self.sending = False
 class PressureSocketHandler(tornado.websocket.WebSocketHandler):
     @gen.coroutine
@@ -74,7 +102,7 @@ class PressureSocketHandler(tornado.websocket.WebSocketHandler):
             pressure = self.sense.get_pressure()
             yield self.write_message(str(pressure))
             print(str(pressure))
-            yield gen.sleep(2)
+            yield gen.sleep(10)
     def open(self):
         print("Pressure socket opened")
         self.sense = SenseHat()
@@ -102,7 +130,7 @@ class OrientationSocketHandler(tornado.websocket.WebSocketHandler):
 
             orientations = pitch + " " + roll + " " + yaw
             yield self.write_message(orientations)
-            yield gen.sleep(2)
+            yield gen.sleep(0.5)
     def open(self):
         print("Orientation socket opened")
         self.sense = SenseHat()
@@ -122,14 +150,14 @@ class AccelerationSocketHandler(tornado.websocket.WebSocketHandler):
     def async_write(self):
         print("ACCELERATION ASYNC FUNC CALLED")
         while(self.sending):
-            acceleration = sense.get_accelerometer_raw()
+            acceleration = self.sense.get_accelerometer_raw()
             x = str(acceleration['x'])
             y = str(acceleration['y'])
             z = str(acceleration['z'])
 
             accelerations = x + " " + y + " " + z
             yield self.write_message(accelerations)
-            yield gen.sleep(2)
+            yield gen.sleep(0.5)
     def open(self):
         self.sense = SenseHat()
         self.sense.clear()
@@ -152,7 +180,8 @@ class Application(tornado.web.Application):
             (r'/pressure', PressureSocketHandler),
             (r'/humidity', HumiditySocketHandler),
             (r'/orientation', OrientationSocketHandler),
-            (r'/acceleration', AccelerationSocketHandler)
+            (r'/acceleration', AccelerationSocketHandler),
+            (r'/irtemp', IRTemperatureSocketHandler)
         ]
 
         settings = {
