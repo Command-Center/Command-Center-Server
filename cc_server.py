90ec37523ef3dc9a1bf69ea34b576fe2989da6f1
Steinar Malin <sma118@post.uit.no>
2018-10-16 23:15:04 +0000

    dunno

--- cc_server.py
+++ cc_server.py
@@ -2,11 +2,12 @@ import paho.mqtt.client as mqtt
 import asyncio
 import threading
 import time
+import queue
 
 class MQTT(threading.Thread):
     mqtt_address = "40.113.99.5"
     connected = False
-    def __init__(self):
+    def __init__(self, message_queue):
         threading.Thread.__init__(self)
         client = mqtt.Client("some_id...")
     def run(self):
@@ -19,9 +20,16 @@ class MQTT(threading.Thread):
                 self.connected = False
                 print("Trying to reconnect")
             time.sleep(1)
+    def subscribe(self):
+        return None
+    def publish(self):
+        return None
+    def isConnected(self):
+        return self.connected
 def main():
+    messageQueue = queue.Queue
     print("Creating mqtt class..")
-    mqtt_thread = MQTT()
+    mqtt_thread = MQTT(message_queue)
     mqtt_thread.start()
     print("Program end")
 if __name__ == "__main__":
